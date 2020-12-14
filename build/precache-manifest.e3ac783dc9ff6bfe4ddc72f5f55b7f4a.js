8752380495661ec4e6689a1b3b195e39e1bcb5e4
Preethi <preethivijayapuri@gmail.com>
2020-12-14 15:21:10 -0600

    first commit

---
+++ build/precache-manifest.e3ac783dc9ff6bfe4ddc72f5f55b7f4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd0284374ff6b3a153d57e9e3d86e414",
+    "url": "/index.html"
+  },
+  {
+    "revision": "35ef5f8bebec4f47cbaf",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "f4f234c47dfca8fe5360",
+    "url": "/static/js/2.3ef2d018.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.3ef2d018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35ef5f8bebec4f47cbaf",
+    "url": "/static/js/main.2f10626a.chunk.js"
+  },
+  {
+    "revision": "f3260a36ed25041221ee",
+    "url": "/static/js/runtime-main.b584fe3d.js"
+  }
+]);
